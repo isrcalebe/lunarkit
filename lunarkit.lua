433e7fd0d4fa91f7aeab26c091c0ac3f66695d3c
isrcalebe <work@isrcalebe.cc>
2024-11-24 22:13:17 -0300

    feat: implement ansi colors

--- lunarkit.lua
+++ lunarkit.lua
@@ -237,3 +237,381 @@ lk_dump_print = function(object, options)
 end
 
 --#endregion
+
+--#region LunarKit ANSI
+
+local LK_ANSI_ENV = {
+  enabled = false,
+  html_tags = false,
+  cache = true,
+  __color_tag = '$%b{}',
+  __reset_cmd = 'reset font_0',
+  __palette = {},
+}
+
+local LK_ANSI_SGR = {
+  --- Attributes
+  reset = 0,
+  normal = 0,
+
+  bold = 1,
+  bold_off = 22,
+
+  intense = 1,
+  intense_off = 22,
+  faint = 2,
+  faint_off = 22,
+
+  dim = 2,
+  dim_off = 22,
+  italic = 3,
+  italic_off = 23,
+
+  oblique = 3,
+  oblique_off = 23,
+
+  underline = 4,
+  underline_off = 24,
+
+  blink = 5,
+  blink_off = 25,
+
+  slow_blink = 5,
+  slow_blink_off = 25,
+
+  rapid_blink = 6,
+  rapid_blink_off = 25,
+
+  inverse = 7,
+  inverse_off = 27,
+
+  hide = 8,
+  hide_off = 28,
+
+  conceal = 8,
+  reveal = 28,
+
+  cross_out = 9,
+  cross_out_off = 29,
+
+  strikethrough = 9,
+  strikethrough_off = 29,
+
+  --- Fonts
+
+  primary_font = 10,
+
+  font_0 = 10,
+  font_1 = 11,
+  font_2 = 12,
+  font_3 = 13,
+  font_4 = 14,
+  font_5 = 15,
+  font_6 = 16,
+  font_7 = 17,
+  font_8 = 18,
+  font_9 = 19,
+
+  black_letter = 20,
+  black_letter_off = 23,
+
+  --- Additional Attributes
+
+  double_underline = 21,
+  double_underline_off = 24,
+
+  proportional = 26,
+  proportional_off = 50,
+
+  --- Foreground Colors
+
+  fg_black = 30,
+  fg_red = 31,
+  fg_green = 32,
+  fg_yellow = 33,
+  fg_brown = 33,
+  fg_blue = 34,
+  fg_magenta = 35,
+  fg_cyan = 36,
+  fg_white = 37,
+  fg_default = 39,
+
+  --- Background Colors
+
+  bg_black = 40,
+  bg_red = 41,
+  bg_green = 42,
+  bg_yellow = 43,
+  bg_brown = 43,
+  bg_blue = 44,
+  bg_magenta = 45,
+  bg_cyan = 46,
+  bg_white = 47,
+  bg_default = 49,
+
+  --- Additional Less Supported Attributes
+
+  frame = 51,
+  frame_off = 54,
+
+  encircle = 52,
+  encircle_off = 54,
+
+  overline = 53,
+  overline_off = 55,
+
+  default_underline_color = 59,
+
+  --- MinTTY Attributes
+
+  shadow = '1:2',
+  shadow_off = 22,
+
+  solid_underline = '4:1',
+  solid_underline_off = 24,
+
+  wavy_underline = '4:3',
+  wavy_underline_off = 24,
+
+  dotted_underline = '4:4',
+  dotted_underline_off = 24,
+
+  dashed_underline = '4:5',
+  dashed_underline_off = 24,
+
+  overstrike = '8:7',
+  overstrike_off = 28,
+
+  superscript = 73,
+  superscript_off = 75,
+
+  subscript = 74,
+  subscript_off = 75,
+
+  --- Bright Foreground Colors
+
+  bright_fg_black = 90,
+  bright_fg_red = 91,
+  bright_fg_green = 92,
+  bright_fg_yellow = 93,
+  bright_fg_blue = 94,
+  bright_fg_magenta = 95,
+  bright_fg_cyan = 96,
+  bright_fg_white = 97,
+
+  --- Bright Background Colors
+
+  bright_bg_black = 100,
+  bright_bg_red = 101,
+  bright_bg_green = 102,
+  bright_bg_yellow = 103,
+  bright_bg_blue = 104,
+  bright_bg_magenta = 105,
+  bright_bg_cyan = 106,
+  bright_bg_white = 107,
+
+  --- Internal Use
+
+  on = false,
+  bright = false,
+  off = false,
+}
+
+local LK_ANSI_HTML = {
+  ['b'] = '\27[1m',
+  ['/b'] = '\27[22m',
+  ['strong'] = '\27[1m',
+  ['/strong'] = '\27[22m',
+  ['i'] = '\27[3m',
+  ['/i'] = '\27[23m',
+  ['em'] = '\27[3m',
+  ['/em'] = '\27[23m',
+  ['u'] = '\27[4m',
+  ['/u'] = '\27[24m',
+  ['sup'] = '\27[73m',
+  ['/sup'] = '\27[75m',
+  ['sub'] = '\27[74m',
+  ['/sub'] = '\27[75m',
+}
+
+local lk_ansi_to_ansi_memo = setmetatable({}, { __mode = 'v' })
+
+lk_ansi_setup = function()
+  if LK_ANSI_ENV.enabled then return end
+
+  do
+    local color
+
+    for i = 0, 255 do
+      LK_ANSI_SGR['color' .. i] = string.format('38;5;%d', i)
+      LK_ANSI_SGR['bg_color' .. i] = string.format('48;5;%d', i)
+      LK_ANSI_SGR['underline_color' .. i] = string.format('58;5;%d', i)
+    end
+
+    for red = 0, 5 do
+      for green = 0, 5 do
+        for blue = 0, 5 do
+          color = 16 + red * 36 + green * 6 + blue
+
+          LK_ANSI_SGR['rgb' .. red .. green .. blue] =
+            string.format('38;5;%d', color)
+          LK_ANSI_SGR['bg_rgb' .. red .. green .. blue] =
+            string.format('48;5;%d', color)
+          LK_ANSI_SGR['underline_rgb' .. red .. green .. blue] =
+            string.format('58;5;%d', color)
+        end
+      end
+    end
+
+    for i = 0, 23 do
+      color = i + 232
+
+      LK_ANSI_SGR['gray' .. i] = string.format('38;5;%d', color)
+      LK_ANSI_SGR['bg_gray' .. i] = string.format('48;5;%d', color)
+      LK_ANSI_SGR['underline_gray' .. i] = string.format('58;5;%d', color)
+    end
+  end
+
+  LK_ANSI_ENV.enabled = true
+end
+
+lk_ansi_html_tags = function(value)
+  assert(type(value) == 'boolean', 'lk_ansi_html_tags: expected a boolean')
+
+  LK_ANSI_ENV.html_tags = value
+end
+
+---Converts a color string to an ANSI escape sequence.
+---
+---@param color string The color string to convert.
+---@return string ansi The ANSI escape sequence.
+local lk_ansi_internal = function(color)
+  if not LK_ANSI_ENV.enabled then return '' end
+
+  color = tostring(color or '')
+
+  if LK_ANSI_ENV.cache and lk_ansi_to_ansi_memo[color] then
+    return lk_ansi_to_ansi_memo[color].value
+  end
+
+  local format = color
+    :gsub('%f[%w]bright%s+', 'bright_')
+    :gsub('%f[%w]bg%s+', 'bg_')
+    :gsub('%s+off%f[%W]', '_off')
+
+  local buffer = {}
+
+  for word in format:gmatch('[%S]+') do
+    if LK_ANSI_SGR[word] then
+      buffer[#buffer + 1] = LK_ANSI_SGR[word]
+    elseif LK_ANSI_ENV.__palette[word] then
+      buffer[#buffer + 1] = LK_ANSI_ENV.__palette[word] ~= ''
+          and LK_ANSI_ENV.__palette[word]
+        or nil
+    elseif word:find('^fg_#%x%x%x%x%x%x$') then
+      local red, green, blue = word:match('^fg_#(%x%x)(%x%x)(%x%x)')
+
+      buffer[#buffer + 1] = string.format(
+        '38;2;%d;%d;%d',
+        tonumber(red, 16),
+        tonumber(green, 16),
+        tonumber(blue, 16)
+      )
+    elseif word:find('^bg_#%x%x%x%x%x%x$') then
+      local red, green, blue = word:match('^bg_#(%x%x)(%x%x)(%x%x)')
+
+      buffer[#buffer + 1] = string.format(
+        '48;2;%d;%d;%d',
+        tonumber(red, 16),
+        tonumber(green, 16),
+        tonumber(blue, 16)
+      )
+    elseif word:find('^=[%d:;]+$') then
+      buffer[#buffer + 1] = word:match('^=([%d:;]+)$')
+    else
+      error(
+        'lk_ansi: invalid token "' .. word .. '" in color "' .. format .. '"',
+        2
+      )
+    end
+  end
+
+  local result = #buffer > 0 and '\27[' .. table.concat(buffer, ';') .. 'm'
+    or ''
+
+  if LK_ANSI_ENV.cache then lk_ansi_to_ansi_memo[color] = { value = result } end
+
+  return result
+end
+
+lk_ansi_raw_paint = function(format, ...)
+  format = select('#', ...) == 0 and '' .. format
+    or table.concat({ format, ... })
+  format = format:gsub(LK_ANSI_ENV.__color_tag, function(s)
+    return lk_ansi_internal(s:sub(3, -2))
+  end)
+
+  return LK_ANSI_ENV.html_tags
+      and (format:gsub('%b<>', function(s)
+        return not LK_ANSI_ENV.enabled and LK_ANSI_HTML[s:sub(2, -2)] and ''
+          or LK_ANSI_HTML[s:sub(2, -2)]
+          or s
+      end))
+    or format
+end
+
+lk_ansi_paint = function(...)
+  local format = lk_ansi_raw_paint(...)
+  local reset = lk_ansi_internal(LK_ANSI_ENV.__reset_cmd)
+
+  return format == '' and reset or reset .. format .. reset
+end
+
+lk_ansi_no_paint = function(format, ...)
+  format = select('#', ...) == '0' and '' .. format
+    or table.concat({ format, ... })
+  format = format:gsub(LK_ANSI_ENV.__color_tag, ''):gsub('\27%[[%d:;]*m"', '')
+
+  return LK_ANSI_ENV.html_tags
+      and (format:gsub('%b<>', function(s)
+        return LK_ANSI_HTML[s:sub(2, -2)] or s
+      end))
+    or format
+end
+
+lk_ansi = setmetatable({}, {
+  __call = function(_, ...)
+    return lk_ansi_paint(...)
+  end,
+  __index = function(self, key)
+    local value = LK_ANSI_ENV[key]
+
+    if value ~= nil then
+      if key == '__palette' then value = {} end
+
+      self[key] = value
+
+      return value
+    end
+
+    local meta
+    meta = {
+      __call = function(_, format, ...)
+        format = select('#', ...) == 0 and '' .. format
+          or table.concat({ format, ... })
+
+        return format ~= '' and lk_ansi_paint(lk_ansi_internal(key) .. format)
+          or lk_ansi_internal(LK_ANSI_ENV.__reset_cmd)
+      end,
+      __index = function(_, sub_key)
+        key = key .. ' ' .. sub_key
+
+        return setmetatable({}, meta)
+      end,
+    }
+
+    return setmetatable({}, meta)
+  end,
+})
+
+--#endregion
